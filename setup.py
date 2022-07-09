77fb201248a6cf91a1b3971ed5462733272c6164
Alessandro Bisiani <alessandro.bisiani.1@gmail.com>
2022-07-09 22:24:53 +0200

    Make piebreak script the entry point once installed.

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(name='pie_break',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/pie_break-run'],
+      scripts=['scripts/piebreak'],
       zip_safe=False)
