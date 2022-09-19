4e6706c8ed7cbc4a5e3130aaf7a182f4ea0f8598
Alessandro Bisiani <alessandro.bisiani.1@gmail.com>
2022-09-19 12:24:08 +0200

    feat: Add a cli for scheduling, and make it downloadable
    sched will not directly work with dynamic updates, and is
    behaving strangely with threading. To my chagrin, this iteration
    uses sched and doesn't attempt any advanced scheduling.
    
    It is the first useful version of piebreak. Coming iterations
    may add support for different playlist specification and
    schedules with APScheduling, or maybe a QT5 GUI.
    
    aside: Could be an opportunity to try using redis?

--- pie_break/pie_break.py
+++ pie_break/pie_break.py
@@ -9,28 +9,26 @@ from threading import current_thread
 class PieBreak(threading.Thread):
     """A timer class which accepts custom work times and lists of urls."""
 
-    s = sched.scheduler(time.time, time.sleep)
-
     def __init__(
         self,
-        start_time=time.time(),
-        work_minutes=50,
-        urls=["https://www.youtube.com/watch?v=6VB4bgiB0yA&ab_channel=Fireplace4K"],
-        event_queue=[],
+        periodic={50},
+        one_off=set(),
+        urls=["https://open.spotify.com/album/6H5fUQk4FkT9iR4SNHnK3D?si=N52exCBWSZW2ee4Pj8oTwg"]
         ):
-        threading.Thread.__init__(self, group=None, target=self, name='pie_thread')
-        self.start_time = start_time
-        self.work_minutes = work_minutes
+        threading.Thread.__init__(self, group=None, target=self, name="pie_thread")
+        self.start_time = time.time()
+        self.periodic = periodic
+        self.one_off = one_off
         if urls:
             self.urls = urls
-        # self.daemon = True
-        self.event_queue = event_queue
+        # Flag for thread survival
+        self.timing = False
 
     def get_params(self):
         """Get the relative starting time in seconds, the total work
         time, and list of urls from which the timer will pick.
         """
-        return self.start_time, self.work_minutes, self.urls, self.event_queue
+        return self.start_time, self.urls, self.periodic, self.one_off, self.timing
 
     # def pause(self):
     #     # Time with asynchio
@@ -54,7 +52,7 @@ class PieBreak(threading.Thread):
             return random.choice(self.urls)
         else:
             try:
-                with open('urls.txt', 'r') as links_file:
+                with open("urls.txt", "r") as links_file:
                     links = links_file.readlines()
                     links = [link.rstrip("\n") for link in links]
             except FileNotFoundError:
@@ -62,40 +60,43 @@ class PieBreak(threading.Thread):
                 return self.urls[0]
             else:
                 # if the file could be found and parsed, return a url at random
-                print('Reading urls from file..')
+                print("Reading urls from file..")
                 if len(links) > 0:
                     return random.choice(links)
 
     def start_break(self):
         """The attention grabbing mechanism."""
-        print(f"\n{self.work_minutes} minutes have passed. Time for a break.\nPieBreak: ", end='')
-        url = self.get_random_url()
-        print(url)
+        if self.timing:
+            print(f"\nTime for a break.\nPieBreak: ", end="")
+            url = self.get_random_url()
+            print(url)
         # webbrowser.open(url)
 
-    def add_scheduled_action(self, delay, priority, action, argument=(), kwargs={}):
-        """Update the schedule on the fly."""
-        # for event in self.s.queue:
-        #     self.s.enterabs(event[0], event[1], event[2], event[3], event[4])
-        self.s.enter(delay, priority, action, argument, kwargs)
-        print(self.s.queue)
-        self.s.run()
+    def set_timing(self, timing_toggle):
+        self.timing = timing_toggle
+        return self.timing
 
     def run(self):
         """Repeatedly open a url after a user-specified number of minutes."""
-        print(f'\r\n{time.strftime("%H:%M:%S", time.localtime())}: Working for {self.work_minutes} minutes')
 
-        # Start an event queue, checking for a queue carried over.
-        if not self.event_queue:
-            self.event_queue.append((self.work_minutes, 1, self.start_break))
-            print(f"1: {self.event_queue}")
-        # Read the latest queue and run it
-        for i in range(10):
-            print(f"Before for loop: {self.s.queue}")
-            for event in self.event_queue:
-                print(f"Event in queue: {event}")
-                self.s.enter(event[0], event[1], self.start_break)
-            print(f"After for loop: {self.s.queue}")
-            self.s.run()
+        s = sched.scheduler(time.time, time.sleep)
 
-        # x = threading.Thread(target=self.schedule, args=(2, 1, self.start_break), name="pie_scheduler", daemon=True)
+        # If there's anything in one_off, join it to periodic
+        # Create and run a schedule with both
+        # Loop over periodic thereafter
+        self.timing = True
+        if self.one_off:
+            events = self.one_off | self.periodic
+            for event_time in events:
+                s.enter(event_time, 1, self.start_break)
+            print(f"\r\n{time.strftime('%H:%M:%S', time.localtime())}: Starting timer..")
+            s.run()
+        if self.periodic:
+            while self.periodic:
+                if self.timing:
+                    for event_time in self.periodic:
+                        s.enter(event_time, 1, self.start_break)
+                        s.run()
+            print(f"\r\n{time.strftime('%H:%M:%S', time.localtime())}: Starting timer..")
+            s.run()
+
